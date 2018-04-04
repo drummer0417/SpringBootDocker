14c86275b486fea155cdb2ccad096861b235b6eb
Hans van Meurs <hans.vanmeurs@wipro.com>
2018-04-04 13:39:56 +0200

    Initial commit

---
+++ src/main/java/nl/androidappfactory/SfgDockerCourseApplication.java
@@ -0,0 +1,12 @@
+package nl.androidappfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDockerCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDockerCourseApplication.class, args);
+	}
+}
---
+++ src/main/java/nl/androidappfactory/controllers/IndexController.java
@@ -0,0 +1,30 @@
+package nl.androidappfactory.controllers;
+
+import nl.androidappfactory.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 1/20/16.
+ */
+@Controller
+public class IndexController {
+
+    private ProductService productService;
+
+    @Autowired
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping({"/", "index"})
+    public String getIndex(Model model){
+
+        model.addAttribute("products", productService.listProducts());
+
+        return "index";
+    }
+
+}
